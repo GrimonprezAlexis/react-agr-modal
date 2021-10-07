45196cde9fe8a7603012f96d28c3c8defaf07714
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-10-07 23:59:25 +0200

    Init Modal component

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-modal-component-library
+A library of React components created using `create-react-app`.
+
+## Installation
+Run the following command:
+npm install react-modal-component-library`
---
+++ src/lib/components/Modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Modal.scss';
+
+export const Modal = ({ show, close, title, body }) => {
+  return (
+    <div className="modal modal__wrapper"
+      style={{
+        transform: show ? 'translateY(0vh)' : 'translateY(-100vh)',
+        opacity: show ? '1' : '0'
+      }}
+    >
+      <div className="modal__header">
+        <p>{title}</p>
+        <span onClick={close} className="close-modal-btn">x</span>
+      </div>
+      <div className="modal__content">
+        <div className="modal__body">
+          <p>{body}</p>
+        </div>
+        <div className="modal__footer">
+          <button onClick={close} className="btn-cancel">Close</button>
+        </div>
+      </div>
+    </div>
+  )
+};
